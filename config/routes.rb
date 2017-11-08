031ae90180c8e136ca4bbd988d6924148427db82
Narendra Maharjan <narendramaharjan90s@gmail.com>
2017-11-08 07:50:26 +0000

    added blogging items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
+
+  get'/post' => 'posts#index'
   get'/about' => 'pages#about'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
